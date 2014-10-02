c8b7e5074217258543823ae9b2d868cb532f9711
David Hsu <dvdhsu@gmail.com>
2014-10-02 02:05:17 -0700

    Add random vegeterian options as well.
    Previously, only random meat options were supported. This adds more
    variety for vegeterians.
    
    The body has also been removed from the vegeterian options. This is in
    light of the new change (where both veg and meat options are pushed
    together, so there's no space for the body).

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ end
 c = College.create!(name: "st-hughs")
 
 # Hugh's mealtimes are fixed
-start_date = DateTime.new(2014, 9, 18)
+start_date = DateTime.new(2014, 9, 28)
 end_date = DateTime.new(2015, 9, 18)
 
 start_date.upto(end_date) do |date|
@@ -49,6 +49,14 @@ start_date.upto(end_date) do |date|
     return lunch_titles[index], lunch_descs[index]
   end
 
+  def random_veg
+    lunch_titles = ["vegetable lasanga", "nut roast with Yorkshire pudding", "cheese, potato, and onion strudel",
+                    "mushroom risotto", "root vegetable gratin", "breaded quorn escalope", "cheese tortellini",
+                    "mushroom and pepper stroganoff", "rost vegetable encroute", "leek and cheese crumble"]
+    index = rand(10)
+    return lunch_titles[index], ""
+  end
+
   lunch = c.meals.create!(meal_type: "lunch", start_date: date.change(hour: 12, min: 30), end_date: date.change(hour: 13))
 
   lunch_notify_time = lunch.start_date.advance(hours: -3)
@@ -62,22 +70,26 @@ start_date.upto(end_date) do |date|
   RoostWorker.perform_at(dinner_notify_time, dinner.id)
 
   lunch_meat = random_meat
+  lunch_veg = random_veg
+
   lunch.menu_items.create!(price_cents: 190,
                            food_type: "main_meat",
                            title: lunch_meat[0],
                            body: lunch_meat[1])
-  lunch.menu_items.create!(price_cents: 150, food_type: "main_veg",
-                           title: "quorn chilli nachos cheese bake", 
-                           body: "It's like meat, but not.")
+  lunch.menu_items.create!(price_cents: 150,
+                           food_type: "main_veg",
+                           title: lunch_veg[0],
+                           body: lunch_veg[1])
 
   dinner_meat = random_meat
+  dinner_veg = random_veg
 
   dinner.menu_items.create!(price_cents: 190,
                            food_type: "main_meat",
                            title: dinner_meat[0],
                            body: dinner_meat[1])
-  dinner.menu_items.create!(price_cents: 150, food_type: "main_veg",
-                           title: "quorn chilli nachos cheese bake", 
-                           body: "It's like meat, but not.")
-
+  dinner.menu_items.create!(price_cents: 190,
+                           food_type: "main_veg",
+                           title: dinner_veg[0],
+                           body: dinner_veg[1])
 end
